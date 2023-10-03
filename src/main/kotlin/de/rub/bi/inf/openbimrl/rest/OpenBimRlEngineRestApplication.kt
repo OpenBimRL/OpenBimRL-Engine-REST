2e335b3d7b082ba157171f478c89031466f3af8a
Becker <beckefp6@ruhr-uni-bochum.de>
2023-10-03 11:46:30 +0200

    initialized spring

---
+++ src/main/kotlin/de/rub/bi/inf/openbimrl/rest/OpenBimRlEngineRestApplication.kt
@@ -0,0 +1,11 @@
+package de.rub.bi.inf.openbimrl.rest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenBimRlEngineRestApplication
+
+fun main(args: Array<String>) {
+	runApplication<OpenBimRlEngineRestApplication>(*args)
+}
---
+++ src/test/kotlin/de/rub/bi/inf/openbimrl/rest/OpenBimRlEngineRestApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.rub.bi.inf.openbimrl.rest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpenBimRlEngineRestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
