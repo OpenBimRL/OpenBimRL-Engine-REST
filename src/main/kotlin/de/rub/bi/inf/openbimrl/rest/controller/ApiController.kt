5e1d37e17a44de617e4c06d85577dbf9df2534c5
Florian Becker <info@florianbecker.eu>
2023-12-19 20:03:48 +0100

    minor changes

--- src/main/kotlin/de/rub/bi/inf/openbimrl/rest/controller/ApiController.kt
+++ src/main/kotlin/de/rub/bi/inf/openbimrl/rest/controller/ApiController.kt
@@ -11,8 +11,7 @@ import kotlin.io.path.name
 @RestController
 class ApiController @Autowired constructor(private val fileService: TemporaryFileService) {
 
-    class ApiAnswer <T> (answer: T, status: String = "ok") {
-        val status = status
+    class ApiAnswer <T> (answer: T, val status: String = "ok") {
         val content = answer
     }
 
@@ -29,7 +28,7 @@ class ApiController @Autowired constructor(private val fileService: TemporaryFil
     @GetMapping("/model/{uuid}")
     fun exec(@PathVariable uuid: UUID): ResponseEntity<ApiAnswer<String?>> {
         val files = fileService.filesWithGlob("${uuid}.ifc")
-        if (files.size == 0) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiAnswer(null, "not found"))
+        if (files.isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiAnswer(null, "not found"))
         val content = Files.readAllLines(files[0], Charsets.UTF_8).reduce { acc, string -> acc + string }
         return ResponseEntity.status(HttpStatus.OK).body(ApiAnswer(content))
     }
